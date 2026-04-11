42c04e20c107d65aac5d83913d8716e3a5c6d862
jimmy opot alando <jimmyopot18@gmail.com>
2026-04-11 22:49:05 +0300

    W06 Code: Trees

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -9,13 +9,16 @@ public class Node
         this.Data = data;
     }
 
+    // PROBLEM 1: Insert UNIQUE values only
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        if (value == Data)
+        {
+            return; // ignore duplicates
+        }
 
         if (value < Data)
         {
-            // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
@@ -23,7 +26,6 @@ public class Node
         }
         else
         {
-            // Insert to the right
             if (Right is null)
                 Right = new Node(value);
             else
@@ -31,15 +33,24 @@ public class Node
         }
     }
 
+    // PROBLEM 2: Contains (recursive search)
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+            return Left != null && Left.Contains(value);
+
+        return Right != null && Right.Contains(value);
     }
 
+    // PROBLEM 4: Height of tree
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
